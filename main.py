ad30b6730387b967debeca7178ec3a3afc160712
Awwab <mawwabkhank2006@gmail.com>
2024-02-25 22:07:07 +0500

    entry box and button

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import PIL
 import tkinter as tk
-from PIL import Image
-from PIL import ImageTk
-from PIL import ImageFilter
+from PIL import ImageDraw, ImageTk, ImageFont, Image
+from tkinter import colorchooser
 from tkinter import filedialog
 
 
@@ -14,25 +12,28 @@ class App:
         
         self.panelB = tk.Frame(self.window)
         self.panelB.grid(row=1, column=6, padx=20, pady=20)
-        tk.Label(self.panelB, text='Click the button below to upload image').pack()
-        tk.Button(self.panelB, text='Upload', command=self.upload).pack()
-        tk.Button(self.panelB, text='Close Image', command=self.close_img).pack()
+        tk.Button(self.panelB, text='Upload', command=self.upload).grid(row=1, column=0, padx=10, pady=10)
+        tk.Button(self.panelB, text='Close Image', command=self.close_img).grid(row=1, column=1, padx=10, pady=10)
+        self.watermark_text = tk.Entry(self.panelB)
+        self.watermark_text.grid(row=2, column=0, padx=10, pady=10)
+        tk.Button(self.panelB, text='Apply Watermark', command=self.apply_watermark).grid(row=2, column=1, padx=10, pady=10)
+        tk.Button(self.panelB, text='Choose Color', command=self.choose_color).grid(row=2, column=2, padx=10, pady=10)
         self.window.mainloop()
 
     def upload(self):
         self.close_img()
         f_types = [('Jpg Files', '*.jpg'),('PNG Files','*.png')] 
         path = filedialog.askopenfilename(filetypes=f_types)
-        self.o_image = Image.open(path) 
+        image = Image.open(path)
 
         # Set the desired width
         new_width = 600  # replace with your desired width
 
         # Calculate the proportional height
-        original_width, original_height = self.o_image.size
+        original_width, original_height = image.size
         new_height = int(original_height * (new_width / original_width))
-
-        image_tk = ImageTk.PhotoImage(self.o_image.resize((new_width, new_height)))
+        self.o_image = image.resize((new_width, new_height))
+        image_tk = ImageTk.PhotoImage(self.o_image)
 
         self.panelA = tk.Label(image=image_tk, borderwidth=5, relief="sunken")
         self.panelA.image = image_tk
